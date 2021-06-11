25c2dc7a5de7295a03007a02a5948b8ee07549fa
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-06-11 16:43:14 +0530

    Update trim_1.py

--- bot/plugins/trim_1.py
+++ bot/plugins/trim_1.py
@@ -1,6 +1,7 @@
 import asyncio
 
-from pyrogram import filters, ForceReply
+from pyrogram import filters
+from pyrogram.types import ForceReply
 
 from ..utils import sample_fn
 from ..screenshotbot import ScreenShotBot
