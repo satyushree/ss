ccde9524b90ed00c890b731639ccbd5e8d49ff63
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-06-11 16:26:06 +0530

    Update sample.py

--- bot/plugins/sample.py
+++ bot/plugins/sample.py
@@ -6,6 +6,6 @@ from ..utils import sample_fn
 from ..screenshotbot import ScreenShotBot
 
 
-@ScreenShotBot.on_callback_query(Filters.create(lambda _, query: query.data.startswith('smpl')))
+@ScreenShotBot.on_callback_query(filters.create(lambda _, query: query.data.startswith('smpl')))
 async def _(c, m):
     asyncio.create_task(sample_fn(c, m))
